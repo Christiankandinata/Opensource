dd140dc90d9af0f273a92e812911e4788e5d9e06
christian kandinata <christian.kandinata@gmail.com>
2022-02-05 22:24:54 +0700

    add docker-compose.yaml and data_ingestion_local.py

---
+++ Data-Engineering/Week2/Airflow/dags/dags_1__Ingest_data_into_local_postgres_using_airflow/data_ingestion_local.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+local_workflow = DAG(
+    "LocalIngestionDag"
+)
+
+with local_workflow:
+
+    wget_task = BashOperator(
+        task_id='wget'
+        , bash_command='echo "hello world"'
+    )
+
+    ingest_task = BashOperator(
+        task_id='ingest'
+        , bash_command='echo "hello world again"'
+    )
+
+    wget_task >> ingest_task
